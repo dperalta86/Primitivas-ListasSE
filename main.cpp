c7db2123bb8a2882624763e2d17a2c9639830479
dperalta <dp25443@gmail.com>
2024-10-28 16:09:40 -0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+using namespace std;
+
+struct NodoListaSE {
+    int info;
+    NodoListaSE* sgte;
+};
+
+void insertarNodoAlFinal(NodoListaSE*& lista, int dato);
+void insertarNodoAlPrincipio(NodoListaSE*& lista, int dato);
+void mostrarLista(NodoListaSE* lista);
+void borrarPrimero(NodoListaSE*& lista);
+void borrarUltimo(NodoListaSE*& lista);
+
+int main() {
+    NodoListaSE* lista = nullptr;
+
+    cout << " ### Carga inicial de la lista ###" << endl << endl;
+    insertarNodoAlFinal(lista, 121);
+    insertarNodoAlFinal(lista, 122);
+    insertarNodoAlFinal(lista, 123);
+    insertarNodoAlPrincipio(lista, 98);
+    insertarNodoAlPrincipio(lista, 99);
+    insertarNodoAlPrincipio(lista, 96);
+    mostrarLista(lista);
+
+    cout << "Borro primer elemento de la lista..." << endl;
+    borrarPrimero(lista);
+    mostrarLista(lista);
+
+    cout << "Borro ultimo elemento de la lista.." << endl;
+    borrarUltimo(lista);
+    mostrarLista(lista);
+
+    return 0;
+}
+
+void insertarNodoAlPrincipio(NodoListaSE*& lista, int dato) {
+    NodoListaSE* aux;
+    aux = lista;
+    lista = new NodoListaSE();
+    lista->info = dato;
+    lista->sgte = aux;
+    return;
+}
+
+void insertarNodoAlFinal(NodoListaSE*& lista, int dato) {
+    NodoListaSE* aux = nullptr;
+    if(!lista) {
+        lista = new NodoListaSE ();
+        lista->info = dato;
+        lista->sgte = nullptr;
+    }
+    else {
+        aux = lista;
+        while (aux->sgte != nullptr)
+            aux = aux->sgte;
+        aux->sgte = new NodoListaSE();
+        aux = aux->sgte;
+        aux->info = dato;
+        aux->sgte = nullptr;
+    }
+    return;
+}
+
+void borrarPrimero(NodoListaSE*& lista) {
+    if (lista != nullptr) {
+        NodoListaSE* aux = lista;
+        lista = lista->sgte; //Acá SI necesito modificar puntero inicial!
+        delete aux;
+    }
+    return;
+}
+
+void borrarUltimo(NodoListaSE*& lista) {
+    NodoListaSE* NodoABorrar = lista;
+    NodoListaSE* anterior = nullptr;
+    if(lista!=nullptr) {
+        while (NodoABorrar->sgte) {
+            anterior = NodoABorrar;
+            NodoABorrar = NodoABorrar->sgte;
+        }
+        if(NodoABorrar != lista) {
+            delete NodoABorrar;
+            anterior->sgte = nullptr;
+        }
+        else
+            borrarPrimero(lista);
+    }
+    return;
+}
+
+void mostrarLista(NodoListaSE* lista) {
+    if (!lista) {
+        cout << " La lista está vacia..." << endl;
+        return;
+    }
+    while (lista) // Recorro la lista, porque es una copia del puntero
+    {
+        cout << "Nodo en la dirección: " << lista << ", con valor en campo Info: " << lista->info <<endl;
+        lista = lista->sgte;
+    }
+    return;
+}
